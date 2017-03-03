2b55b1bc39931029adce8be4fa9c21a6d15e7171
Jeremy Ward <jeremy2929@twc.com>
2017-03-03 13:26:46 -0600

    Changed background and some styling in photos

--- js/aboutMe.js
+++ js/aboutMe.js
@@ -3,17 +3,13 @@ import { Link } from 'react-router'
 
 
 export default React.createClass({
-  getIntialState() {
-    var bodyElement = document.getElementsByClassName("bodyPage")
-    bodyElement.className = "bioBodyPage"
-  },
+
   render() {
-    var bodyElement = document.getElementsByClassName("bodyPage")
-    console.log(bodyElement);
-    bodyElement.className = "bioBodyPage"
+  ;
     return (
       <div className="bioPage">
         <section className="bioSection">
+
             <p className="bioText">   I am native Texan, originally from Houston and also lived for a while in Rosenberg outside of Houston.  I came to San Antonio in 2008, indirectly because of Hurricane Ike. I have been writing code almost my entire life.  I have 10 years experience in Computer Operations of mainframes, Network Administration, and PICK BASIC programming.  I am passionate about logic-oriented and mathematical programming challenges. I also enjoy reading, camping, road trips, motorcycling, windsurfing and sailing.</p>
         </section>
       </div>

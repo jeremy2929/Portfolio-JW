feb57774bb57fb02a963af74c22913364c84374a
Jeremy Ward <jeremy2929@twc.com>
2017-03-04 00:23:38 -0600

    Responsive bio pic

--- js/aboutMe.js
+++ js/aboutMe.js
@@ -9,8 +9,9 @@ export default React.createClass({
     return (
       <div className="bioPage">
         <section className="bioSection">
-
-            <p className="bioText">   I am native Texan, originally from Houston and also lived for a while in Rosenberg outside of Houston.  I came to San Antonio in 2008, indirectly because of Hurricane Ike. I have been writing code almost my entire life.  I have 10 years experience in Computer Operations of mainframes, Network Administration, and PICK BASIC programming.  I am passionate about logic-oriented and mathematical programming challenges. I also enjoy reading, camping, road trips, motorcycling, windsurfing and sailing.</p>
+          <div className="bioBackground">
+            <p className="bioText">   I am native Texan, originally from Houston and also lived for a while in Rosenberg outside of Houston.  I came to San Antonio in 2008, indirectly because of Hurricane Ike. I have been writing code almost my entire life.  I have 10 years experience in Computer Operations of mainframes, Network Administration, and PICK BASIC programming.  I recently completed a 12 week intense bootcamp at the Iron Yard, studying HTML, CSS, and JavaScript.  I am passionate about logic-oriented and mathematical programming challenges. I also enjoy reading, camping, road trips, motorcycling, windsurfing and sailing.</p>
+          </div>
         </section>
       </div>
     )
